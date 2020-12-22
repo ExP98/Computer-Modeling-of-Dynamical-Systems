fce99d3155bcb3b0faf152543a4a057c433b36c2
Egor Glushkov <egorglushkov2014@yandex.ru>
2020-12-22 21:24:53 +0300

    logistic lattice

--- logistic_lattice_pause.py
+++ logistic_lattice_pause.py
@@ -1,23 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
-import time
 
-w, h = 100, 100
+w, h = 128, 128
 # cp in [0; 1000], gn in [250; 1000]
-gn, cp = 631, 711
-r = gn / 250
-img = Image.new('RGB', (w, h))
-pixels = img.load()
+gn, cp = 650, 650
+r, c_div, step_lim = gn / 250, 750, 500
 matrix = np.random.random_sample((h, w))
-print(matrix)
 
-for k in range(15):
-    copy_m = np.copy(matrix)
+fig, ax = plt.subplots()
+
+for k in range(step_lim):
+    ax.cla()
+    plt.suptitle(f"Logistic lattice, step {k+1}, CP = {cp}, GN = {gn}")
+
+    copy_m = matrix.copy()
     for i in range(h):
         for j in range(w):
             avg = (copy_m[i - 1, j] + copy_m[(i + 1) % h, j] + copy_m[i, j - 1] + copy_m[i, (j + 1) % w]) / 4
-            c = cp * (avg - copy_m[i, j]) / 1000
+            c = cp * (avg - copy_m[i, j]) / c_div
             x = r * copy_m[i, j] * (1 - copy_m[i, j]) + c
             if x > 1:
                 matrix[i, j] = 1
@@ -25,10 +25,6 @@ for k in range(15):
                 matrix[i, j] = 0
             else:
                 matrix[i, j] = x
-            clr = int(matrix[i, j] * 255)
 
-            pixels[i, j] = (clr, clr, 0)
-    print(matrix)
-    img.show()
-
-print('end')
+    ax.imshow(matrix, interpolation='nearest', vmin=0, vmax=1)
+    plt.pause(0.005)
--- test.py
+++ test.py
@@ -1,30 +1,13 @@
+from PIL import Image, ImageFont, ImageDraw
 import numpy as np
-import matplotlib.pyplot as plt
 
-w, h = 128, 128
-# cp in [0; 1000], gn in [250; 1000]
-gn, cp = 650, 650
-r, c_div, step_lim = gn / 250, 750, 500
-matrix = np.random.random_sample((h, w))
-
-fig, ax = plt.subplots()
-
-for k in range(step_lim):
-    ax.cla()
-    plt.suptitle(f"Logistic lattice, step {k+1}, CP = {cp}, GN = {gn}")
-
-    copy_m = matrix.copy()
-    for i in range(h):
-        for j in range(w):
-            avg = (copy_m[i - 1, j] + copy_m[(i + 1) % h, j] + copy_m[i, j - 1] + copy_m[i, (j + 1) % w]) / 4
-            c = cp * (avg - copy_m[i, j]) / c_div
-            x = r * copy_m[i, j] * (1 - copy_m[i, j]) + c
-            if x > 1:
-                matrix[i, j] = 1
-            elif x < 0:
-                matrix[i, j] = 0
-            else:
-                matrix[i, j] = x
-
-    ax.imshow(matrix, interpolation='nearest', vmin=0, vmax=1)
-    plt.pause(0.005)
+h, w = 100, 100
+img = Image.new('RGB', (w, h))
+# draw = ImageDraw.Draw(img)
+pixels = img.load()
+num, blob_size = 50, 10
+for x in range(-blob_size, blob_size):
+    for y in range(-blob_size, blob_size):
+        pixels[num+x, num+y] = (100000, 255, 0)
+ImageDraw.Draw(img).text((0, 90), f"Julia, w {w}, h = {h}", (255, 255, 255))
+img.show()
