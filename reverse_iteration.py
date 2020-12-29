21593f8be25afca55fbcd010b51d29421bc9f8f7
Egor Glushkov <egorglushkov2014@yandex.ru>
2020-12-29 21:14:46 +0300

    little fixes

--- reverse_iteration.py
+++ reverse_iteration.py
@@ -1,35 +1,40 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 
-c = 0.285 + 0.01j
+c_list = [(-0.7055, -0.3842)]
+# c_list = [(-0.7055, -0.3842), (-0.22, -0.74), (-0.8, 0.156), (-0.7, 0.27015), (0.285, 0.01), (-0.0085, 0.71), (-0.12375, 0.56508), (-0.12, 0.74), (-0.39054, -0.58679), (-1.25, 0.0), (0.11031, -0.67037), (-0.194, 0.6557)]
+w, h = 1000, 1000
+border = 2
 
-counter = 0
-points_list = []
 
-
-def calculation(_z, ctr):
-    new_z = (_z - c) ** 0.5
+def calculation(_z, _c, ctr):
+    new_z = (_z - _c) ** 0.5
     points_list.append(new_z)
     points_list.append(-new_z)
     ctr += 1
     if ctr < 20:
-        calculation(new_z, ctr)
-        calculation(-new_z, ctr)
+        calculation(new_z, _c, ctr)
+        calculation(-new_z, _c, ctr)
 
 
-calculation(c, counter)
+def visualization_rev(_c):
+    calculation(_c, _c, 0)
 
-w, h = 1000, 1000
-img = Image.new('RGB', (w, h))
-pixels = img.load()
+    img = Image.new('RGB', (w, h))
+    pixels = img.load()
+    mx = [[0 for _ in range(w)] for _ in range(h)]
+    for pt in points_list:
+        x = round((pt.real / (2 * border) + 0.5) * w)
+        y = round((pt.imag / (2 * border) + 0.5) * h)
+        mx[x][y] += 1
+    for x_coord in range(w):
+        for y_coord in range(h):
+            color = mx[x_coord][y_coord]
+            pixels[x_coord, y_coord] = (color, color, color)
+    ImageDraw.Draw(img).text((10, h - 20), f"Julia, c = {_c}", (255, 255, 255))
+    img.show()
+    # img.save('reverse_iter/rev_' + str(_c) + '.png')
 
-mx = [[0 for _ in range(w)] for _ in range(h)]
-for pt in points_list:
-    x = round((pt.real / 3 + 0.5) * w)
-    y = round((pt.imag / 3 + 0.5) * h)
-    mx[x][y] += 1
-for x_coord in range(w):
-    for y_coord in range(h):
-        color = mx[x_coord][y_coord]
-        pixels[x_coord, y_coord] = (color, color, color)
-img.show()
-# img.save('reverse_iter/rev_20_coloring.png')
+
+for c in c_list:
+    points_list = []
+    visualization_rev(c[0] + 1j * c[1])
--- test.py
+++ test.py
@@ -1,35 +1,44 @@
 from math import cos, sin, pi
 import matplotlib.pyplot as plt
 
+
+K = 0.6
+def another_func(pt):
+    x = pt[0] + K * sin(pt[1])
+    y = pt[1] + x
+    return x, y
+
+
 par1, par2 = 0.7, -0.82
 def func(pt):
     return par1 * pt[0] + pt[1], par2 + pt[0] ** 2
+
 
 def distance(pt1, pt2):
     x1, y1 = pt1[0], pt1[1]
     x2, y2 = pt2[0], pt2[1]
     return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
-h = 0.5
-p1 = (-1, -1)
+h = 0.05
+p1 = (1, -1)
 p2 = (1, 1)
-pts_list = []
+pts_set = set()
 fig, ax = plt.subplots()
 
 def fragmentation(pt1, pt2):
     d = distance(pt1, pt2)
-    print(d)
+    # print(d)
     if d < h:
-        pts_list.append(pt1)
-        pts_list.append(pt2)
-    else:
-        plt.scatter([x[0] for x in pts_list], [x[1] for x in pts_list])
-        plt.draw()
+        pts_set.add(pt1)
+        pts_set.add(pt2)
+    elif d < 5:
+        # plt.scatter([x[0] for x in pts_set], [x[1] for x in pts_set])
+        # plt.draw()
         pt3 = ((pt1[0] + pt2[0]) / 2, (pt1[1] + pt2[1]) / 2)
         fragmentation(func(pt1), func(pt3))
         fragmentation(func(pt2), func(pt3))
 
 fragmentation(p1, p2)
-print(pts_list)
-plt.scatter([x[0] for x in pts_list], [x[1] for x in pts_list])
+print(len(pts_set))
+plt.scatter([x[0] for x in pts_set], [x[1] for x in pts_set], s=1)
 plt.show()
